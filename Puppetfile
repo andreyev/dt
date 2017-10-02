c5ba8860f1f603619e82f5a683cc5d9abc902a64
Andreyev Dias de Melo <andreyev.dias@adtsys.com.br>
2017-10-01 22:48:45 -0300

    WIP

--- Puppetfile
+++ Puppetfile
@@ -3,5 +3,6 @@ forge "https://forgeapi.puppetlabs.com"
 
 mod 'garethr-docker', '5.3.0'
 mod 'stahnma-epel', '1.2.2'
-mod 'puppet-letsencrypt', '2.0.1'
-mod 'puppet-nginx', '0.7.1'
+# LE dont work with AZ or AWS public DNS
+#mod 'puppet-letsencrypt', '2.0.1'
+#mod 'puppet-nginx', '0.7.1'
--- default.pp
+++ default.pp
@@ -1,28 +1,26 @@
 class { 'docker': }
 package { 'unzip': }
-class { 'nginx': }
-class { '::letsencrypt':
-  configure_epel => true,
-  email          => 'noreply@example.com',
-}
-letsencrypt::certonly { "${domain}":
-  manage_cron          => true,
-  cron_before_command  => '/sbin/service nginx stop',
-  cron_success_command => '/sbin/service nginx restart',
-}
-# wont work on AWS Public DNS because of
-# https://community.letsencrypt.org/t/policy-forbids-issuing-for-name-on-amazon-ec2-domain/12692/5
-nginx::resource::server { 'owncloud.ssl-proxy':
-  listen_port  => 80,
-  ssl_redirect => true,
-  server_name  => ["${domain}"],
-  ssl_port     => 443,
-  proxy        => 'http://localhost:443',
-  ssl          => true,
-  ssl_cert     => "/etc/letsencrypt/live/${domain}/fullchain.pem",
-  ssl_key      => "/etc/letsencrypt/live/${domain}/privkey.pem",
-}
-
+# LE dont work with AZ or AWS public DNS
+#class { 'nginx': }
+#class { '::letsencrypt':
+#  configure_epel => true,
+#  email          => 'noreply@example.com',
+#}
+#letsencrypt::certonly { "${domain}":
+#  manage_cron          => true,
+#  cron_before_command  => '/sbin/service nginx stop',
+#  cron_success_command => '/sbin/service nginx restart',
+#}
+#nginx::resource::server { 'owncloud.ssl-proxy':
+#  listen_port  => 80,
+#  ssl_redirect => true,
+#  server_name  => ["${domain}"],
+#  ssl_port     => 443,
+#  proxy        => 'http://localhost:443',
+#  ssl          => true,
+#  ssl_cert     => "/etc/letsencrypt/live/${domain}/fullchain.pem",
+#  ssl_key      => "/etc/letsencrypt/live/${domain}/privkey.pem",
+#}
 class {'docker::compose':
   ensure       => present,
   install_path => '/bin',
